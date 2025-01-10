c75773af2ddba3c4375b38719e103a1c557ae0aa
takenokoya9973 <62060755+takenoko9973@users.noreply.github.com>
2025-01-10 18:06:48 +0000

    nodeを介さずにdownloadできる関数追加

--- pygrdm/grdm.py
+++ pygrdm/grdm.py
@@ -23,9 +23,19 @@ class GRDMClient:
             "Authorization": f"Bearer {self._token}"
         }
 
+    def fetch_file_list(self, node_file: NodeFile) -> NodeFilesList | None:
+        url = node_file.get_files_list_url(domain=self._domain)
+
+        with requests.get(url, headers=self._headers, timeout=2000) as response:
+            if response.status_code != HTTPStatus.OK:
+                print("情報の取得に失敗しました。ステータスコード:", response.status_code)
+                print("レスポンス:", response.text)
+                return None
+
+            return NodeFilesList(response.json())
+
     def fetch_node_file(self, node: str, osf_path: str | Path) -> NodeFile | None:
-        if isinstance(osf_path, str):
-            osf_path = Path(osf_path)
+        osf_path = Path(osf_path)
 
         now_node_file = NodeFile.create_root(node, "osfstorage")
         directries = osf_path.parts
@@ -45,32 +55,25 @@ class GRDMClient:
 
         return now_node_file
 
-    def fetch_file_list(self, node_file: NodeFile) -> NodeFilesList | None:
-        url = node_file.get_files_list_url(domain=self._domain)
-
-        with requests.get(url, headers=self._headers, timeout=2000) as response:
-            if response.status_code != HTTPStatus.OK:
-                print("情報の取得に失敗しました。ステータスコード:", response.status_code)
-                print("レスポンス:", response.text)
-                return None
-
-            return NodeFilesList(response.json())
-
     def download_node(self, node_file: NodeFile, filename: str | Path | None = None) -> None:
         url = node_file.get_download_url(domain=self._domain)
         print(f"Downloading {url}")
 
-        if filename is None:
-            filename = Path(node_file.name)
-        elif filename is str:
-            filename = Path(filename)
+        # 保存先指定なしの場合、保存名はファイル名になる
+        filename = Path(node_file.name) if filename is None else Path(filename)
 
-        with requests.get(url, headers=self._headers, stream=True, timeout=2000) as response:
+        with (
+            filename.open(mode="wb") as save_file,
+            requests.get(url, headers=self._headers, stream=True, timeout=2000) as response,
+        ):
             total_size = int(response.headers.get("content-length", 0))
-            with (
-                filename.open(mode="wb") as f,
-                tqdm(total=total_size, unit="B", unit_scale=True) as pbar,
-            ):
+            with tqdm(total=total_size, unit="B", unit_scale=True) as pbar:
                 for chunk in response.iter_content(chunk_size=8192):
-                    f.write(chunk)
+                    save_file.write(chunk)
                     pbar.update(len(chunk))
+
+    def download_file(
+        self, node: str, osf_path: str | Path, filename: str | Path | None = None
+    ) -> None:
+        node_file = self.fetch_node_file(node, osf_path)
+        self.download_node(node_file, filename=filename)
